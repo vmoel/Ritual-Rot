2d23190b5a15310bfa1527950ef4300791020316
Maria-user1 <mahe33@elev.tec.dk>
2025-09-12 12:49:10 +0200

    kasse kan bevæge sig!:)

--- Ritual Rot 2d/Assets/spiller_script.cs
+++ Ritual Rot 2d/Assets/spiller_script.cs
@@ -1,16 +1,27 @@
+using System.Numerics;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+
 
 public class spiller_script : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    float horizontal;
+    float vertical;
+    private Rigidbody2D rb;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new UnityEngine.Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
     }
 }
