3c37dfa938b96ab7806d255afbaf01471a6ec4c8
Maria-user1 <mahe33@elev.tec.dk>
2025-09-25 22:47:40 +0200

    Den ser lidt nasty ud med dialogen virker!!

--- Ritual Rot 2d/Assets/ending1.cs
+++ Ritual Rot 2d/Assets/ending1.cs
@@ -1,16 +1,41 @@
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ending1 : MonoBehaviour
+public class Ending1 : MonoBehaviour
 {
+    public Text Hovedtext;
+    public Button Interknap;
+
+    historien nuværendeBlock;
+
+    static historien block4 = new historien("Nu er det slut", "slut");
+    static historien block3 = new historien("yubi", "næste", block4);
+    static historien block2 = new historien("Nummer 1 kult entusiast", "næste", block3);
+    static historien block1 = new historien("Du valgte at være kult fan!!", "næste", block2);
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        HvisBlock(block1);
     }
 
-    // Update is called once per frame
-    void Update()
+    void HvisBlock(historien block)
     {
-        
+        Hovedtext.text = block.skriften;
+        Interknap.GetComponentInChildren<Text>().text = block.næste;
+
+        nuværendeBlock = block;
     }
+
+    public void berørtknap()
+    {
+        HvisBlock(nuværendeBlock.næsteknapBlock);
+    }
+    
+
+
+
 }
--- Ritual Rot 2d/Assets/interaktion.cs
+++ Ritual Rot 2d/Assets/interaktion.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Data;
 using Unity.Multiplayer.Center.Common.Analytics;
 using Unity.VisualScripting;
 using UnityEditor.VersionControl;
@@ -7,22 +9,86 @@ using UnityEngine.UI;
 
 public class interaktion : MonoBehaviour
 {
-    public GameObject Interact;
-    void OnTriggerEnter(Collider other)
-        {
+    public GameObject DialogPanel;
+    public Text DialogText;
+    public string[] dialog;
+    private int index;
 
-        }
+    public GameObject næstek;
 
+    public float ordHastighed;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public bool Nærspiller;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E) && Nærspiller)
+        {
+            if (DialogPanel.activeInHierarchy)
+            {
+                ingenT();
 
+            }
+            else
+            {
+                DialogPanel.SetActive(true);
+                StartCoroutine(skriver());
+            }
+        }
+
+        if (DialogText.text == dialog[index])
+        {
+            næstek.SetActive(true);
+        }
     }
+
+    public void ingenT()
+    {
+        DialogText.text = "";
+        index = 0;
+        DialogPanel.SetActive(false);
+    }
+    IEnumerator skriver()
+    {
+        foreach (char letter in dialog[index].ToCharArray())
+        {
+            DialogText.text += letter;
+            yield return new WaitForSeconds(ordHastighed);
+        }
+    }
+
+    public void NæsteLinje()
+    {
+        næstek.SetActive(false);
+        if (index < dialog.Length - 1)
+        {
+            index++;
+            DialogText.text = "";
+            StartCoroutine(skriver());
+        }
+        else
+        {
+            ingenT();
+        }
+    }
+
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Nærspiller = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Nærspiller = false;
+            ingenT();
+        }
+    }
+
 }
